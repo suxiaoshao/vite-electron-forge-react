859099342cd4c485de1cbe018609daee9bb847e9
苏炜杰 <suweijie@bytedance.com>
2021-07-25 01:39:06 +0800

    chore: 添加 eslint prettier

--- main/src/index.ts
+++ main/src/index.ts
@@ -1,30 +1,29 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit();
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
 }
 
 const createWindow = (): void => {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800,
-    });
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+  });
 
+  if (app.isPackaged) {
+    const url = path.resolve(__dirname, '../renderer/index.html');
+    mainWindow.loadFile(url);
+  } else {
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:3030');
+  }
 
-    if(app.isPackaged){
-        const url=path.resolve(__dirname,'../renderer/index.html')
-        mainWindow.loadFile(url);
-    }else{
-        // and load the index.html of the app.
-        mainWindow.loadURL("http://localhost:3030");
-    }
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -36,17 +35,17 @@ app.on('ready', createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
